666eab6352ba4231e053b113289a7b4a8d94d1ce
luchopal <lucho005>
2017-05-16 20:18:26 -0300

    Guardar el apellido del usuario generado

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    params.require(:user).permit(:name, :surname,:email, :password,:password_confirmation)
   end
 
   def create
