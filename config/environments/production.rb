b3455c5758af599f2b1e084dd03f7e092f6c8c2b
a983060 <lpalmieri@anses.gov.ar>
2020-01-15 09:25:35 -0300

    Reemplazo envio de mail de gmail a mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,15 +91,15 @@ Rails.application.configure do
   }
 
   config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :user_name            => ENV['GMAIL_USER'],
-      :password             => ENV['GMAIL_PASSWORD'],
-      :authentication       => "plain",
-      :enable_starttls_auto => true
-  }
+        address: "smtp.mailgun.org",
+        authentication: :plain,
+        port: 587,
+        domain: ENV['MAILGUN_DOMAIN'],
+        user_name: ENV['MAILGUN_LOGIN'],
+        password: ENV['MAILGUN_PASSWORD'],
+        api_key: ENV['MAILGUN_API_KEY'],
+    }
 
   config.i18n.default_locale = :es
 end
